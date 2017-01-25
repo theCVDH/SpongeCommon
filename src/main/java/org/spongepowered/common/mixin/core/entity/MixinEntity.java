7beb51e3e991afa2bd503a2e9d9fa2952b4301bd
Me4502 <mnmiller1@hotmail.com>
2017-01-25 15:26:47 +1000

    Formatting.

--- src/main/java/org/spongepowered/common/mixin/core/entity/MixinEntity.java
+++ src/main/java/org/spongepowered/common/mixin/core/entity/MixinEntity.java
@@ -372,7 +372,7 @@ public abstract class MixinEntity implements IMixinEntity {
 
             phaseData.context.getOwner().ifPresent(owner -> causeBuilder.named(NamedCause.owner(owner)));
 
-            destructCause = causeBuilder.build();
+            this.destructCause = causeBuilder.build();
         }
     }
 
